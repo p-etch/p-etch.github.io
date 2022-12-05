aa1621d4daccbdac324b13691defad7f03913498
Patrick Etchison <jmetchison11@gmail.com>
2022-12-05 14:45:15 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-// Set the date we're counting down to
+// Set the date
 var countDownDate =  new Date("January 1, 2023 18:30:00").getTime();
 
-// Update the count down every 1 second
+// Update the count down
 var x = setInterval(function() {
-    // Get today's date and time
     var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
     var distance = countDownDate - now;
-
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
@@ -19,7 +14,7 @@ var x = setInterval(function() {
     document.getElementById("cd-timer").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
   
-    // If the count down is finished, write some text
+    // If the count down is finished, write congrats
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("cd-timer").innerHTML = "Congrats!";
@@ -27,57 +22,38 @@ var x = setInterval(function() {
   
 }, 10);
 
-// Set the date we're counting down to
 var countDownDate2 =  new Date("December 13, 2022 18:00:00").getTime();
 
-// Update the count down every 1 second
 var x = setInterval(function() {
-    // Get today's date and time
     var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
     var distance = countDownDate2 - now;
-
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  
-    // Display the result in the element with id="cd-timer"
+
     document.getElementById("cd-timer2").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
   
-    // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("cd-timer2").innerHTML = "Congrats!";
     }
-  
 }, 10);
 
-// Set the date we're counting down to
+
 var countDownDate3 =  new Date("December 25, 2022 12:00:00").getTime();
-
-// Update the count down every 1 second
 var x = setInterval(function() {
-    // Get today's date and time
     var now = new Date().getTime();
-  
-    // Find the distance between now and the count down date
     var distance = countDownDate3 - now;
-
-    // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
   
-    // Display the result in the element with id="cd-timer"
     document.getElementById("cd-timer3").innerHTML = days + "d " + hours + "h "
     + minutes + "m " + seconds + "s ";
   
-    // If the count down is finished, write some text
     if (distance < 0) {
         clearInterval(x);
         document.getElementById("cd-timer3").innerHTML = "Congrats!";
