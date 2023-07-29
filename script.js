96e841c623abf9e4c944763da7b995e633f8c927
Patrick Etchison <jmetchison11@gmail.com>
2023-07-29 19:31:22 -0400

    Update countdown timers and labels

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Set the date
-var countDownDate =  new Date("July 25, 2023 18:30:00").getTime();
+var countDownDate =  new Date("November 28, 2023 12:00:00").getTime();
 
 // Update the count down
 var x = setInterval(function() {
@@ -22,7 +22,7 @@ var x = setInterval(function() {
   
 }, 10);
 
-var countDownDate2 =  new Date("March 24, 2023 12:00:00").getTime();
+var countDownDate2 =  new Date("August 10, 2023 12:00:00").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
