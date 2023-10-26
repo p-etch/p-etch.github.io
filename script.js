cfe60ad085f3b773246f148ced471806342e9447
Patrick Etchison <jmetchison11@gmail.com>
2023-10-26 16:39:34 -0400

    Update Image and Timers

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ var x = setInterval(function() {
   
 }, 10);
 
-var countDownDate2 =  new Date("August 10, 2023 12:00:00").getTime();
+var countDownDate2 =  new Date("November 23, 2023 12:00:00").getTime();
 
 var x = setInterval(function() {
     var now = new Date().getTime();
@@ -42,7 +42,7 @@ var x = setInterval(function() {
 }, 10);
 
 
-var countDownDate3 =  new Date("December 25, 2022 00:00:00").getTime();
+var countDownDate3 =  new Date("October 28, 2022 00:00:00").getTime();
 var x = setInterval(function() {
     var now = new Date().getTime();
     var distance = countDownDate3 - now;
